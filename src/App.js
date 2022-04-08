7b209b26b99b9416e0a0f5e6223e79ccdb212863
Luis Salcedo Salas <rideluis@gmail.com>
2022-04-07 19:24:37 -0500

    feat: setup router in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo!</h1>
-      <p>Es el React de assessment</p>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hola Mundo!</h1>
+        <p>Es el React de assessment</p>
+      </div>
+    </Router>
   );
 }
 
